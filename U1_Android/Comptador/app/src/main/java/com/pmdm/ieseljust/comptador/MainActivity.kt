16cbf240261085d950c8c77a7c8d7032f7b793b4
Jose A. <joamuran@gmail.com>
2024-09-19 10:20:39 +0200

    Added U1/Comptador Project

---
+++ U1_Android/Comptador/app/src/main/java/com/pmdm/ieseljust/comptador/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.pmdm.ieseljust.comptador
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private var comptador=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Referencia al TextView
+        val textViewContador=findViewById<TextView>(R.id.textViewComptador)
+
+        // Inicialitzem el TextView amb el comptador a 0
+        textViewContador.text=comptador.toString() // Estem fent una assignacio directament o accedinta algun metode?
+
+        // Referencia al botón
+        val btAdd=findViewById<Button>(R.id.btAdd)
+
+        // Asociaciamos una expresióin lambda como
+        // respuesta (callback) al evento Clic sobre
+        // el botón
+        btAdd.setOnClickListener {
+            comptador++
+            textViewContador.text=comptador.toString()
+        }
+
+    }
+}
